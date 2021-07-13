743fd4cbd641ef8080da23540fc4d8b346a9cfcf
drevanthkrishna <78145539+drevanthkrishna@users.noreply.github.com>
2021-07-13 20:58:56 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,24 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+    attach(body){
+        this.sling.bodyA = body;
+    }
+    
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    display(){
+        imageMode(CENTER)
+        image(polygon_img,polygon.position.x,polygon.position.x,40,40)
+    }
+}
